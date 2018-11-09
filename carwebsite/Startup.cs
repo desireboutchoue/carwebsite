8b38c755042c8a4c6d1821d433c171d08fa3873a
LAPTOP-A7L8MF79\Dez <angeboutchoue@gmail.com>
2018-11-09 17:41:35 +0300

    car

---
+++ carwebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(carwebsite.Startup))]
+namespace carwebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
